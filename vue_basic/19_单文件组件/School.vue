84f4f45a28e46c91b709fa3f1699d66f70998390
范剑虹 <123@abc.com>
2023-10-26 08:12:59 +0800

    单文件组件

---
+++ vue_basic/19_单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showInfo">点我提示学校名</button>
+    </div>
+</template>
+
+<script>
+    //组件交互相关的代码
+    export default {
+        name:'School',
+        data() {
+            return {
+                name:'啦啦啦',
+                address:'北京昌平'
+            }
+        },
+        methods: {
+            showInfo(){
+                alert(this.name)
+            }
+        },
+    }
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: orange;
+    }
+</style>
