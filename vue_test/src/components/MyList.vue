23998643d59090b5bb81194402c73a56f37b51c1
范剑虹 <123@abc.com>
2023-10-28 23:13:13 +0800

    插件、局部样式

---
+++ vue_test/src/components/MyList.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul class="todo-main">
+        <MyItem v-for="item in todos" :key="item.id" :todo="item" />
+    </ul>
+</template>
+
+<script>
+import MyItem from "./MyItem";
+
+export default {
+    name: "MyList",
+    components: { MyItem },
+    data() {
+        return {
+            todos: [
+                { id: "001", title: "抽烟", done: true },
+                { id: "002", title: "喝酒", done: false },
+                { id: "003", title: "开车", done: true }
+            ],
+        };
+    },
+};
+</script>
+
+<style>
+/*main*/
+.todo-main {
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+}
+
+.todo-empty {
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+}
+</style>
--- vue_test/src/main.js
+++ vue_test/src/main.js
@@ -3,10 +3,7 @@ import Vue from 'vue'
 //引入APP
 import App from './App.vue'
 
-import {mixin,mixin2} from './mixin'
-
 Vue.config.productionTip = false
-Vue.mixin(mixin,mixin2)
 
 //创建VM
 new Vue({
