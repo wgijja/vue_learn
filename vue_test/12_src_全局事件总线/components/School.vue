68179ec7d2717cff43f492830ab26e060834d0f8
范剑虹 <123@abc.com>
2023-10-31 08:00:37 +0800

    全局事件总线

---
+++ vue_test/12_src_全局事件总线/components/School.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="school">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "School",
+    data() {
+        return {
+            name: "幼儿园来啦地地地",
+            address: "北京朝阳",
+        };
+    },
+    mounted() {
+        this.$bus.$on('hello', (val) => {
+            console.log("我是学校组件，我接收到了数据：", val);
+        });
+    },
+};
+</script>
+
+<style scoped>
+.school {
+    background-color: skyblue;
+    padding: 5px;
+    margin-top: 30px;
+}
+</style>
---
+++ vue_test/src/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="school">
+        <h2>学校名称：{{ name }}</h2>
+        <h2>学校地址：{{ address }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "School",
+    data() {
+        return {
+            name: "幼儿园来啦地地地",
+            address: "北京朝阳",
+        };
+    },
+    mounted() {
+        this.$bus.$on('hello', (val) => {
+            console.log("我是学校组件，我接收到了数据：", val);
+        });
+    },
+    beforeDestroy(){
+        //销毁组件时同时销毁自定义事件
+        this.$bus.$off('hello')
+    }
+};
+</script>
+
+<style scoped>
+.school {
+    background-color: skyblue;
+    padding: 5px;
+    margin-top: 30px;
+}
+</style>
