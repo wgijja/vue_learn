8352506ab9abb5e10f8a1cd51a5afdde7f77a3d2
范剑虹 <123@abc.com>
2023-11-02 08:08:57 +0800

    发送请求与代理

--- vue_test/vue.config.js
+++ vue_test/vue.config.js
@@ -1,5 +1,26 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  //配置代理服务器方式一，缺点是只能配置一个代理且资源优先本地
+  /* devServer:{
+    proxy:'http://localhost:5000'
+  } */
+  //配置代理服务器方式2
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        pathRewrite:{'^/api':''},
+        ws: true,//用于支持websocket
+        changeOrigin: true //用于控制请求中的host值
+      },
+      '/2api': {
+        target: 'http://localhost:5001',
+        pathRewrite:{'^/2api':''},
+        ws: true,//用于支持websocket
+        changeOrigin: true //用于控制请求中的host值
+      },
+    }
+  }
 })
