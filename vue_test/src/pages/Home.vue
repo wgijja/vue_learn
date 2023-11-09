850f9fc99580b9dfb2b5a59b20f6d726ad658dee
范剑虹 <123@abc.com>
2023-11-09 23:26:25 +0800

    路由的基本使用

---
+++ vue_test/src/pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>我是Home的内容</h2>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    beforeDestroy(){
+        console.log('Home组件即将销毁')
+    },
+    mounted(){
+        console.log('Home组件挂载完毕')
+    }
+};
+</script>
+
+<style>
+</style>
