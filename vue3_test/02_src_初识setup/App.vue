fc6812abf2675597eb5174109f65ffe743976fe6
范剑虹 <123@abc.com>
2023-11-15 23:16:57 +0800

    初识vue及ref函数及reactive函数

---
+++ vue3_test/02_src_初识setup/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <h1>我是APP组件</h1>
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <h2>性别：{{ sex }}</h2>
+    <h2>a:{{ a }}</h2>
+    <button @click="sayHello">说3话</button>
+    <button @click="welcome">说2话</button>
+    <button @click="testRead3">在vue2的配置中去读取3的配置</button>
+    <button @click="testRead2">在vue3的setup配置中去读取2的配置</button>
+</template>
+
+<script>
+import { h } from "vue";
+
+export default {
+    name: "App",
+
+    data() {
+        return {
+            sex: "男",
+            a: 100,
+        };
+    },
+    methods: {
+        welcome() {
+            alert("欢迎学习VUe");
+        },
+        testRead3() {
+            console.log(this.sex);
+            console.log(this.name);
+            console.log(this.age);
+            console.log(this.sayHello);
+        },
+    },
+
+    //此处暂时不考虑响应式
+    setup() {
+        //数据
+        let name = "张三";
+        let age = 18;
+        let a = 200;
+
+        //方法
+        function sayHello() {
+            alert(`你好啊，我叫${name},我今年${age}岁了`);
+        }
+
+        function testRead2() {
+            console.log(name);
+            console.log(age);
+            console.log(this.sex);
+            console.log(this.welcome);
+        }
+
+        //返回一个对象（常用）
+        return {
+            name,
+            age,
+            sayHello,
+            testRead2,
+        };
+        //返回一个渲染函数
+        /* return () => {
+            return h("h1", "尚硅谷");
+        }; */
+    },
+};
+</script>
---
+++ vue3_test/03_src_ref函数/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <h1>我是APP组件</h1>
+    <h2>姓名：{{ name }}</h2>
+    <h2>年龄：{{ age }}</h2>
+    <h2>工作种类：{{ job.type }}</h2>
+    <h2>工作薪水：{{ job.salary }}</h2>
+    <button @click="update">修改人的信息</button>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+    name: "App",
+    setup() {
+        //数据
+        let name = ref("张三");
+        let age = ref(18);
+        let job = ref({
+            type: "前端工程师",
+            salary: "30k",
+        });
+
+        //方法
+        function update() {
+            name.value = "李四";
+            age.value = 38;
+            job.value.type = '后端工程师'
+            job.value.salary = '100k'
+        }
+
+        //返回一个对象（常用）
+        return {
+            name,
+            age,
+            update,
+            job,
+        };
+    },
+};
+</script>
